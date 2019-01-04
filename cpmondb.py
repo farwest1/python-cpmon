83da852b9f5e7261ae5d6399f8280a39cd8cbb93
Bernd Möller <bmtrash@web.de>
2019-01-04 16:47:52 +0000

    Avoid connection to DB

--- cpmondb.py
+++ cpmondb.py
@@ -2,12 +2,12 @@ import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
-dbname='enco'
-try:
-    conn = psycopg2.connect(dbname=dbname, user='enco', host='localhost', password='enco')
-    app.logger.info ('Successfully connected to database %s ', dbname)
-except:
-    print ('I am unable to connect to the database')
+# dbname='enco'
+# try:
+#     conn = psycopg2.connect(dbname=dbname, user='enco', host='localhost', password='enco')
+#     app.logger.info ('Successfully connected to database %s ', dbname)
+# except:
+#     print ('I am unable to connect to the database')
 
 
 def getConnectionPointsAll():
